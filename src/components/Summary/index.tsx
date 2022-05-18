ba96f274b3b4c7dbe7b2f4c608702eb97291c4c1
caleb-freitas <calebfreitas@tutanota.com>
2022-05-18 15:22:43 -0300

    feat: add summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import incomeIgm from "../../assets/income.svg"
+import outcomeIgm from "../../assets/outcome.svg"
+import totalIgm from "../../assets/total.svg"
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Income</p>
+          <img src={incomeIgm} alt="income" />
+        </header>
+        <strong>R$ 1.000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Outcome</p>
+          <img src={outcomeIgm} alt="outcome" />
+        </header>
+        <strong>-R$ 500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalIgm} alt="total" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+    </Container>
+  )
+}
